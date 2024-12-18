bac23ffdfc1a7399b907b2befdf5d12b64045217
PramodhaKasam <kasam.pramodha@gmail.com>
2024-12-18 17:07:36 +0530

    update

--- src/components/State.js
+++ src/components/State.js
@@ -4,16 +4,15 @@ import '../styles/State.css'
 const StateCard = ({ temple, onClick }) => {
   return (
     <div className="temple-card">
-      <img
-        src={temple.image}
-        alt={temple.name}
-        className="h-40 w-full object-cover rounded-lg"
-      />
-      <h2 className="text-lg font-bold mt-2">{temple.name}</h2>
-      <button
-        onClick={() => onClick(temple)}
-        className="bg-blue-500 text-white mt-2 px-4 py-2"
-      >
+      <div className="image">
+        <img src={temple.image}  alt={temple.name} />
+        <div className="content">
+          <h1>{temple.name}</h1>
+          <p>{temple.description}</p>
+        </div>
+      </div>
+      <h2>{temple.name}</h2>
+      <button onClick={() => onClick(temple)}>
         View Temples
       </button>
     </div>
