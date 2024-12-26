5bc76e3839b379f28c81aabedf6acc52ff46c904
PramodhaKasam <kasam.pramodha@gmail.com>
2024-12-26 19:14:19 +0530

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,9 +9,27 @@ import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../components/navbar";
 import temple from "../assets/temple-main.jpg";
 import Footer from "../components/Footer";
+import Swiper from 'swiper'
+import {Navigation, Pagination} from 'swiper/modules'
+import 'swiper/css/bundle'
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+
+    loop:true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable : true,
+      dynamicBullets:true
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
   return (
     <div>
@@ -106,7 +124,51 @@ const HomePage = () => {
       </div>
 
       <div className="what">
-        
+        <h1>Explore Exclusively with Us</h1>
+        <div className="card-wrapper swiper">
+          <div className="swiper-wrapper">
+            <div className="point swiper-slide">
+              <img src="https://as2.ftcdn.net/v2/jpg/06/12/32/45/1000_F_612324540_iVqW5JxR8fS8Vpj6EjQazSCgrcheb4GN.jpg" />
+              <div className="matter">
+                <strong>State-wise Division</strong>
+                <p>
+                  "Effortlessly navigate through temples by selecting the state of your choice. Experience a seamless
+                  search functionality tailored for India’s diverse cultural and spiritual heritage."
+                </p>
+                <p><span>View </span> &#8594;</p>
+              </div>
+              {/* <span className="span">&#8594;</span> */}
+            </div>
+            <div className="point swiper-slide">
+              <img src="https://t4.ftcdn.net/jpg/09/57/81/07/360_F_957810751_w7KdtaHHqd720zJWiXclnOKsmp3S48YG.jpg" />
+              <div className="matter">
+                <strong>Comprehensive Temple Listings</strong>
+                <p>
+                  "Access a meticulously curated list of temples for each state. Whether it’s a famous shrine or a hidden gem,
+                   we’ve got you covered."
+                </p>
+                <p><span>View </span> &#8594;</p>
+              </div>
+              {/* <span className="span">&#8594;</span> */}
+            </div>
+            <div className="point swiper-slide">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2gZ8ifZc5IpHfLqf4VtXldOjrIMMiuarbLw&s" />
+              <div className="matter">
+                <strong>More Than Just Darshan</strong>
+                <p>
+                  "Dive into the rich history and fascinating stories behind each temple. Discover their unique
+                  traditions, architectural marvels, and specialties that make them stand out."
+                </p>
+                <p><span>View </span> &#8594;</p>
+              </div>
+              {/* <span className="span">&#8594;</span> */}
+            </div>
+          </div>
+          <div className="swiper-pagination"></div>
+          <div className="slide-button swiper-button-prev"></div>
+          <div className="slide-button swiper-button-next"></div>
+        </div>  
+        {/* <p className="count">&lt;  01/04  &gt;</p> */}
       </div>
 
       {/* <div className="explore-section">
